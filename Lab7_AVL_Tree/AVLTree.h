a613e633f48b83f2780b20c22ed35be1eac4f91e
Nathan Ramirez <nrami112@xe-02.cs.ucr.edu>
2023-11-12 18:38:57 -0800

    added comments to code

--- Lab7_AVL_Tree/AVLTree.h
+++ Lab7_AVL_Tree/AVLTree.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class AVLTree {
     public:
-        AVLTree() : root(nullptr) 
+        AVLTree() : root(nullptr) // initialize an empty tree
         {
         }
 
@@ -16,19 +16,19 @@ class AVLTree {
 
         int balanceFactor(Node* ); // Return the balance factor of a given node
 
-        void printBalanceFactors(); // Traverse and print the tree in inorder notation. Print the string followed by its balance factor in parentheses followed by a , and one space.
+        void printBalanceFactors(); // Traverse and print the tree in inorder notation. Print the string followed by its balance factor in parentheses followed by a ", "
 
-        void visualizeTree(const string &); 
+        void visualizeTree(const string &); // Generate dotty file and visualize the tree using dotty program. Call this function before and after rotation
 
     private:
 
         Node* root;
-        void rotate(Node* );
-        void rotateLeft(Node *);
-        void rotateRight(Node *);
-        void replaceChild(Node* , Node* , Node* );
-        void setChild(Node *, const string &, Node* );
-        void printBalanceFactors(Node *);
+        void rotate(Node* ); // Implement four possible imbalance cases and update the parent of the given node
+        void rotateLeft(Node *); // Rotate the subtree to left at the given node and returns the new subroot
+        void rotateRight(Node *); // Rotate the subtree to right at the given node and returns the new subroot
+        void setChild(Node *, const string &, Node* ); //private helper to set parameter's parent's left/right to its child 
+        void replaceChild(Node* , Node* , Node* ); // private helper to replace the child of a parent 
+        void printBalanceFactors(Node *);  
         int height(Node *);
         void visualizeTree(ofstream &, Node *);
 
